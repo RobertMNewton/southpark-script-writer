2b3d1b7ea43bacd70ff34f9dc73174998d258805
RobertMNewton <robert.m.newton7@gmail.com>
2024-05-28 20:24:16 +1000

    test tokeniser

--- better_tokens.py
+++ better_tokens.py
@@ -4,6 +4,7 @@ from torch import LongTensor
 import torch
 from typing import List, Dict, Optional, Union
 import pandas as pd
+from tqdm import tqdm
 
 
 class BreakLoopTo(Exception):
@@ -12,16 +13,22 @@ class BreakLoopTo(Exception):
 
 TokeniserData = Dict[Union[int, str], Dict[str, int]]
 
-def train_tokeniser_from_text(text: str, max_iterations: int  = 1000, extra: List[str] = ["<EOS>", "<PAD>", "<CLS>", "<UNK>", "<MASK>", "<BOS>", "<SEP>"]) -> TokeniserData:    
+def train_tokeniser_from_text(text: str, max_iterations: int  = 2000, extra: List[str] = ["<EOS>", "<PAD>", "<CLS>", "<UNK>", "<MASK>", "<BOS>", "<SEP>"]) -> TokeniserData:    
     tokeniser, max_token_length = {1: list(set(text))}, 1
-    for _ in range(max_iterations):
+    
+    pg = tqdm(range(max_iterations), dynamic_ncols=True)
+    for _ in pg:
+        pg.set_description_str(f"Training Tokeniser (N = {sum(len(tokens) for tokens in tokeniser.values())})")
+        pg.set_postfix_str(s = "Tokenising...")
+        
         # tokenise text using greedy tokenisation
         tokenised_text, i = [], 0
         while i < len(text):
+            #print(i, " < ", len(text))
             try:
                 for token_length in range(max_token_length):
                     token_length = max_token_length - token_length
-                    if len(text) - i < token_length:
+                    if len(text) - i < token_length or token_length not in tokeniser:
                         continue
                     
                     for token in tokeniser[token_length]:
@@ -32,6 +39,14 @@ def train_tokeniser_from_text(text: str, max_iterations: int  = 1000, extra: Lis
                             raise BreakLoopTo()
             except BreakLoopTo:
                 continue
+            
+            # if we got here then no token pair fits the text. We should thus add the next char to the token
+            tokenised_text.append(text[i])
+            tokeniser[1].append(text[i])
+            
+            i += 1
+        
+        pg.set_postfix_str(s="Counting frequencies...")
         
         # determine the frequency each token pair occurs    
         pair_freqs: Dict[str, int] = {}
@@ -42,28 +57,33 @@ def train_tokeniser_from_text(text: str, max_iterations: int  = 1000, extra: Lis
             else:
                 pair_freqs[pair] = 1
         
-        # Merge most common token pair! And repeat 
+        pg.set_postfix_str(s="Merging...")
+        
+        # Merge most common token pair! And repeat
         max_freq = max(pair_freqs.values())
         for k, v in pair_freqs.items():
             if v == max_freq:
-                if not len(k) in tokeniser:
-                    tokeniser[len(k)] = []
-                    if len(k) > max_token_length:
-                        max_token_length = len(k)
+                new_token = "".join(k)
+                if not len(new_token) in tokeniser:
+                    tokeniser[len(new_token)] = []
+                    if len(new_token) > max_token_length:
+                        max_token_length = len(new_token)
+                
                 
                 tokeniser[len(k[0])].remove(k[0])
-                tokeniser[len(k[1])].remove(k[1])
+                if k[0] != k[1]:    
+                    tokeniser[len(k[1])].remove(k[1])
+                
+                tokeniser[len(new_token)].append(new_token)
                 
                 if len(tokeniser[len(k[0])]) == 0:
                     tokeniser.pop(len(k[0]))
                 if len(tokeniser[len(k[1])]) == 0:
                     tokeniser.pop(len(k[1]))
-
-                new_token = "".join(k)
-                
-                tokeniser[len(new_token)].append(new_token)
                 
                 break
+    
+    
                 
     tokeniser["extra"] = extra
     tokeniser["token_lengths"] = sorted([k for k in tokeniser.keys() if isinstance(k, int)], reverse=True)
@@ -78,7 +98,7 @@ def train_tokeniser_from_text(text: str, max_iterations: int  = 1000, extra: Lis
         
         current_id += len(tokeniser[k])
         
-    tokeniser["vocab_size"] = current_id
+    tokeniser["vocab_size"] = current_id + len(extra)
     tokeniser["detokeniser"] = detokeniser
     
     return tokeniser
@@ -87,7 +107,7 @@ def train_tokeniser_from_text(text: str, max_iterations: int  = 1000, extra: Lis
 def save_tokeniser(filepath: str, tokeniser: dict) -> None:
     os.makedirs(os.path.dirname(filepath), exist_ok=True)
     with open(filepath, "w") as f:
-        json.jump(tokeniser, f)
+        json.dump(tokeniser, f)
         
         
 def load_tokeniser(filepath: str, tokeniser: dict) -> None:
@@ -128,7 +148,7 @@ for episode in episodes.iloc:
         _scripts.append("\n".join(episode_script))
         
 tokeniser = train_tokeniser_from_text("\n".join(_scripts))
-save_tokeniser("southpark_tokens.json", tokeniser)
+save_tokeniser("./southpark_tokens_all.json", tokeniser)
         
         
     
